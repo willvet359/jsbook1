c1e0c39e31b0fac0ed90145cd997b8307181b7c3
willvet359 <129315974+willvet359@users.noreply.github.com>
2023-09-06 13:52:16 -0700

    more objecting

--- js-files/codefile3.js
+++ js-files/codefile3.js
@@ -1,23 +1,5 @@
 alert("yup");
 
-/* var funnyGuy = {};
-funnyGuy.firstName = "Conan";
-funnyGuy.lastName = "O'Brien";
-
-funnyGuy["firstName"] = "Eddie";
-funnyGuy["lastName"] = "Murphy";
-
-var funnyGuy = {
-    firstName: "Hurricane",
-    lastName: "theDog",
-
-    getName: function() {
-    return "Name is: " + this.firstName + " " + this.lastName;
-    }
-};
-
-alert(funnyGuy.getName()); */
-
 var person = {
     getName: function () {
         return "Name is: " + this.firstName + " " + this.lastName;
