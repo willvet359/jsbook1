934c736fa0d6918ff4c3449eb9d752cf5570684b
willvet359 <129315974+willvet359@users.noreply.github.com>
2023-09-09 10:23:09 -0700

    fixed event handling

--- js-files/codefile-photoflip.js
+++ js-files/codefile-photoflip.js
@@ -1,22 +1,39 @@
 var leftButton = document.querySelector("#left");
 var rightButton = document.querySelector("#right");
 
-leftButton.addEventListener("click", flipPhoto, false);
-window.addEventListener("keydown", flipPhoto, false);
+leftButton.addEventListener("click", setDirectionLeft, false);
+window.addEventListener("keydown", determineDirection, false);
 
-rightButton.addEventListener("click", flipPhoto, false);
-//window.addEventListener("keydown", flipPhoto(), false);
+rightButton.addEventListener("click", setDirectionRight, false);
 
+function setDirectionLeft(e) {
+    console.log("going left");
 
-function flipPhoto(e) {
-    console.log("hm");
-    console.log(e.key);
+    flipPhoto("left");
+}
 
-    var thePicElement = document.querySelector("#thePic");
+function setDirectionRight(e) {
+    console.log("going right");
 
-    
+    flipPhoto("right");
+}
+
+function determineDirection(e) {
     if (e.key == 'ArrowLeft') {
-        thePicElement.setAttribute("src", "resources/Pic10.jpg");
+        flipPhoto("left");
+    } else if (e.key == 'ArrowRight') {
+        flipPhoto("right");
+    }
+}
+
+function flipPhoto(dir) {
+    
+    var thePicElement = document.querySelector("#thePic");
+    
+    if (dir == "left") {
+        thePicElement.setAttribute("src", "resources/Pic04.jpg");
+    } else if (dir == "right") {
+        thePicElement.setAttribute("src", "resources/Pic02.jpg");
     }
     
 }
